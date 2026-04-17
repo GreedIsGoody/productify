c6311f86668f54116db397650f231dd7846916fe
Mikhail Murzakov <121337665+GreedIsGoody@users.noreply.github.com>
2026-04-17 15:43:32 +0300

    clerk dependencies, lucid react and daisyUI integration. Creating a routing and navbar section

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ClerkProvider } from '@clerk/react'
+import {BrowserRouter} from "react-router"
+
+const PUBLISHED_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHED_KEY) {
+  throw new Error("Missing published key")
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHED_KEY}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
   </StrictMode>,
 )
