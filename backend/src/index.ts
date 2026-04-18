d240ef972a9429307f117e6a814b4d574a53bd34
Mikhail Murzakov <121337665+GreedIsGoody@users.noreply.github.com>
2026-04-18 16:14:10 +0300

    user-sync-setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 
 
 
-app.use(cors({origin: ENV.FRONTEND_URL}));
+app.use(cors({origin: ENV.FRONTEND_URL, credentials: true}));
 app.use(clerkMiddleware()) // auth obj will be attached to the request
 app.use(express.json()) //parcing requiest bodies
 app.use(express.urlencoded({extended: true}))
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/react'
-import {BrowserRouter} from "react-router"
+import { BrowserRouter } from "react-router"
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const PUBLISHED_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -11,11 +13,17 @@ if (!PUBLISHED_KEY) {
   throw new Error("Missing published key")
 }
 
+
+// Create a client
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHED_KEY}>
       <BrowserRouter>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </BrowserRouter>
     </ClerkProvider>
   </StrictMode>,
